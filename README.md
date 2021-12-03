92ac2bd0262314eed1d5e42d375eb445b524ff23
PageOfPages <51131231+PageOfPages@users.noreply.github.com>
2021-12-03 06:14:24 -0500

    final commit

---
+++ README.md
@@ -0,0 +1,49 @@
+Neighborhood Search - a data retriever for city data
+=================================================================
+
+Introduction
+============
+
+Neighborhood Search is a simple python program that uses API calls to retrieve 
+data on a specific city. With all important/relevant data at your disposal, 
+you can learn about your city and how you can realistically live there. 
+
+Neighborhood Search provides you with data to show you what your neighborhood 
+will statistically provide for the average resident. Discover if this is 
+an unrealistic town for you to move to or maybe discover the statistics of 
+your town don't represent what you've experienced living there. 
+
+- **General City Statistics:** - Do most people rent in your town and is this rent low? Does minimum wage meet the cost of rent? How long should you expect to be comuting everyday? Are there entry level occupations availble in your city?
+- **Average Costs:** - How much is the cost of Grocceries? Will you be able to afford basics such as bread and eggs? How much is gas? Will you be able to make your comute without breaking your paycheck?
+
+
+Setup
+======
+Clone this git.
+
+In the terminal, navigate to the folder python-project-PageOfPages.
+
+Run the python file neighborhood-search.py in the terminal.
+
+```
+$ python neighborhood-search.py
+
+```
+
+After a short introduction, you will be prompted to enter an address.
+
+
+Future Picture
+==================
+A lot of city's accross America have an ever changing and growing apartment market. But what specific factors influence the price of an apartment? What external factors make an apartment that looks cheap actually a more expensive choice? What are the effects of the construction of new apartments in the center city and gentrification in outer parts of the city? What is the expectancy of the struggling low income workers and the reality of renting in a city today? 
+
+Neighborhood Search will use data retrieval and visualization to highlight the conditions and pressures placed on low income groups looking for housing. What are their best options given current apartment prices versus minimum wage and cost of living. Furthermore, Neighborhood Search will detect where prices are going depending on surrounding factors. Are apartments getting more expensive, and if so, then why? Where are low income groups with no outter family financial support expected to live? And not just "live" in the sense of surviving... **live**.
+
+
+Origin of Concept
+=================
+I got the idea for this project when I was looking at Zillow for apartments and just calculating out the realistic amount I'd need to make to live in an apartment. The math just didn't add up. And I'm an individual, I'm not even an entire family supported by one. I wanted more than anything for the site to turn gray/murky brown, the pictures to change to beat up awkward walls and unsafe/loud surroundings, and the listings to drop to realistic prices that matched typical low income circumstances. I just wanted a parady of this site that gave the reality of what I'd be getting without me having to be taunted by these better apartments that were just enough out of my budget. 
+
+Ideally this project would become a site where you can enter an address and learn about what life you could realistically live there. Can you walk or take public transporatation to a supermarket or laundrymat? Are there local jobs available that are above minimum wage? If not, what is the minimum wage and can you afford rent on that? How many people need to room to lower the cost of rent if possible? How will you afford for a bed and kitchen table and other basic home necessities if this is your first apartment?
+What about the luxaries in life - Are there local Deli's or pizza places or coffee shops? Is there a local gym or park to exercise in? 
+How about general health - how bad is the pollution, how is the tap water (will you need to buy bottle water?), how easy is it to apply for government insurance? Can you afford therapy or over the counter medicine?
--- neighborhood-search.py
+++ neighborhood-search.py
@@ -1,4 +1,13 @@
+from radar import RadarClient
 import requests
+from time import sleep
+
+
+# initialize client with your project's secret key
+SECRET_KEY = "prj_live_sk_9e32c80a5ead6974670df24b831fd00ca48c183e"
+PUB_KEY = "prj_live_pk_b4cede5fbe2f41ae7d329ae59f9a155944fed53e"
+radar = RadarClient(secret_key=SECRET_KEY, pub_key=PUB_KEY)
+
 
 '''given a zipcode, this program returns contextual data on the area such as:
     - local transportation
@@ -13,103 +22,169 @@ import requests
 '''
 
 
+# **** Radar.io API **** #
 '''
 Query User for zipcode. 
-Using Radar.io Geocoding API
     - Read out promt to user explaining what this program does and returning the 
       City and State of the zip code.
     - Return longitude/latitude 
 '''
-def user_location:
+def user_location():
+    print("\n\t\t\t   Neighborhood Search")
+    print("- the best way to know what you should really expect from your Neighborhood. -\n")
+    sleep(5)
+    print("Moving somewhere new? Do you already live somewhere and don't fully get the")
+    print("layout of where you live?\n")
+    sleep(7)
+    print("Or, are you curious about what it's like to live in other areas?\n")
+    sleep(7)
+    print("Neighborhood Search is here to answer these questions by giving you the ")
+    print("relevent data YOU need.\n")
+    sleep(5)
+    print("We hope this can help to educate you, make you more aware, and to inform you!\n")
+    sleep(5)
+
+
+    confirmation = "no"
+    while (confirmation.lower() == "no"):
+        print("Input the Address you'd like to search: ")
+        input_home_address = input().replace(" ", "+")
+
+        site_address ='https://api.radar.io/v1/search/autocomplete?query={}&layers=fine'.format(input_home_address)
+        data = {'Authorization': PUB_KEY}
+        home_address = requests.get(site_address, headers=data)     #Address is a json array of addresses similiar to the address inpu by user. 
+                                                                    #First result is one closests to the users IP address
+
+        confirmation = " "
+        while (confirmation.lower() != "no" and confirmation.lower() != "yes"):
+            print("\nPlease Confirm (Yes/No) if this is your Address:  \n" + home_address.json()['addresses'][0]['formattedAddress'] + "\n\nConfirmation (yes/no): ")  
+            confirmation = input()
+
+        if (confirmation.lower() == "no"):
+            print("\nTo return the address you are looking for, please be more specific. Make sure your")
+            print("spelling is correct and include the State in which the address you are looking for is.\n")
+            # print("and include the State in which the address you are looking for is.\n")
+            sleep(5)
+
+    
+    lat = home_address.json()['addresses'][0]['latitude']
+    lng = home_address.json()['addresses'][0]['longitude']
+    
+    return (lat,lng)
 
 
 '''
 Given long/latitude data (retrieved from user_location once user inputs a zip),
-Return data based on Zip
+Return data based on Address
 '''
-Class Zip_Data:
+class Data:
 
-    '''
-    Local transportation
-    Radar.io API
-    '''
-    def transport:
+    coordinates = user_location()
 
-
-    '''
-    Local food and grocerery shops
-    Radar.io API
-    '''
-    def food:
+    
+    # Currently, in order to get transporatation and food near an address, I need to use Google Places API which costs money
+    # '''
+    # Local transportation
+    # Radar.io API
+    # '''
+    # def transport():
+    #     return 0
+        
+    # '''
+    # Local food and grocerery shops
+    # Radar.io API
+    # '''
+    # def food():
+    #     return 0
 
     #---------------------General City Data----------------------#
-    # Get city data using Data USA API
+    # **** Data USA API **** #
     '''
     City data on race/ethnicity
     '''
-    def race_eth:
+    def race_eth():
+        return 0
 
     '''
     City data on health
     '''
-    def health:
+    def health():
+        return 0
 
     '''
     City data on jobs
     '''
-    def jobs:
+    def jobs():
+        return 0
 
     '''
     City data on comute time
     '''
-    def comute_time:
+    def comute_time():
+        return 0
 
     '''
     City data on *** ect, anything else I might want to add here***
     '''
-    def ect:
+    def ect():
+        return 0
 
     #----------Rent----------#
 
     '''
     City data on percentage of rent vs. owned 
     '''
-    def rent_vs_owned:
+    def rent_vs_owned():
+        return 0
 
     '''
     City data on rent average cost
     '''
-    def rent_cost:
+    def rent_cost():
+        return 0
 
     #-------Economy----------#
 
     '''
     City data on wages 
     '''
-    def wage:
+    def wage():
+        url = "http://api.datausa.io/api/?show=geo&sumlevel=state&required=avg_wage"
 
+        json = requests.get(url).json()
+
+        # data = [dict(zip(json["headers"], d)) for d in json["data"]]
+
+    
     '''
     City data on income by location 
     '''
-    def location_income:
+    def location_income():
+        return 0
 
     '''
     City data on most common occumpations
     Return a list of top 15 ocupation by general category
     '''
-    def occupations:
+    def occupations():
+        return 0
 
+    
+    #--------------------------Costs------------------------------#
+    # **** Grocerybear API **** #
     '''
     Regional data on average grocery costs
     '''
-    def grocery_cost:
+    def grocery_cost():
+        return 0
 
+    # **** HERE Fuel Prices API **** #
     '''
     Regional data on average gas costs
     '''
-    def gas_costs:
+    def gas_costs():
+        return 0
     
-    #------------------------------------------------------------#
 
     '''
     Return apartment lsitings with beds, baths, location based on Zipcode with a 10 mile radius.
@@ -117,7 +192,8 @@ Class Zip_Data:
     Sort listings based on cheapest to most expensive. Give overview of more expensive listing - stating 
     something like "80 apartments in this area priced over $2,000 per month".
     '''
-    def apartment_listings:
+    def apartment_listings():
+        return 0
     
 
 '''
@@ -126,4 +202,24 @@ then create an object of that zip and retrive data
 Formally read out a prompt for each data call such as 
 for occupations say "The top ocuppations in your area are..."
 '''
-def main:
+def main():
+    print("\n----------------------------------------COMPLETE!-----------------------------------------")
+    print("At this stage in the project, I have been able to use Radar API to confirm the address the")
+    print("user puts in. I still have yet to retrieve data from other APIs. Below are API's I plan to  ")
+    print("call for varied data describing the city of the user's inputted address:")
+    print("\t- Data USA API for the following city statistics")
+    print("\t\t- race/ethnicity")
+    print("\t\t- health")
+    print("\t\t- jobs")
+    print("\t\t- comute timee")
+    print("\t\t- rent vs owned")
+    print("\t\t- rent_cost")
+    print("\t\t- wage")
+    print("\t\t- income")
+    print("\t\t- occupation")
+    print("\t- Grocerybear API for average Grocerry costs")
+    print("\t- HERE Fuel Prices API for average Gas costs")
+
+
+if __name__ == "__main__": 
+    main()
